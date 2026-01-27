35a8815443b4996bfdc3af9c5c24b5c49e11ef39
zxy <yimiao.zhou@tiinlab.com>
2026-01-27 15:51:20 +0800

    1月27日第一次冒泡排序

--- more_126/text1.c
+++ more_126/text1.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
-
+void bubble_sort(int* arr,int sz)
+{
+    int i=0;
+    for (i = 0; i < sz; i++)
+    {
+        int j=0;
+        {
+            for(j=0;j<sz-i-1;j++)
+            {
+                if(arr[j]>arr[j+1])
+                {
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+    }
+    
+}
 int main() {
-    printf("Hello, World!\n");
+    int arr[10]={9,8,7,6,5,4,3,2,1,0};
+    int sz=sizeof(arr)/sizeof(arr[0]);
+    bubble_sort(arr,sz);
+    int n=0;
+    for(n=0;n<sz;n++)
+    {
+        printf("%d ",arr[n]);
+    }
     return 0;
 }
